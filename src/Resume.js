70df2c1bb69374e0507b94cba8babe75cbdd812d
justecii <justecii@gmail.com>
2017-11-11 13:36:01 -0800

    style changes, switching to other project for now

--- src/Resume.js
+++ src/Resume.js
@@ -11,7 +11,7 @@ class Resume extends Component {
                     <h2>Experience</h2>
                     <Row>
                         <Col s={4}>
-                        <i class="fa fa-database fa-3x iconStyle" aria-hidden="true"></i>
+                        <i class="fa fa-database fa-5x iconStyle" aria-hidden="true"></i>
                         <ul>
                             <li className='skills'>PostgreSQL</li>
                             <li className='skills'>Sequelize</li>
@@ -20,7 +20,7 @@ class Resume extends Component {
                         </ul>
                         </Col>
                         <Col s={4}>
-                        <i class="fa fa-laptop fa-3x iconStyle" aria-hidden="true"></i>
+                        <i class="fa fa-laptop fa-5x iconStyle" aria-hidden="true"></i>
                         <ul>
                             <li className='skills'>Javascript/jQuery</li>
                             <li className='skills'>ReactJS</li>
@@ -29,7 +29,7 @@ class Resume extends Component {
                         </ul>
                         </Col>
                         <Col s={4}>
-                        <i class="fa fa-code fa-3x iconStyle" aria-hidden="true"></i>
+                        <i class="fa fa-code fa-5x iconStyle" aria-hidden="true"></i>
                         <ul>
                             <li className='skills'>Ruby on Rails</li>
                             <li className='skills'>Git</li>
